4f8c459cc82c16d6761044e412736a51d9809cf5
abdulkarim8293 <abdulkarim8293@gmail.com>
2021-09-01 18:06:47 +0600

    insert user in database

--- app/src/main/java/com/example/mvvm_android/AddUserActivity.kt
+++ app/src/main/java/com/example/mvvm_android/AddUserActivity.kt
@@ -1,11 +1,43 @@
 package com.example.mvvm_android
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.example.mvvm_android.room_database.User
+import com.example.mvvm_android.room_database.UserDao
+import com.example.mvvm_android.room_database.UserDatabase
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class AddUserActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_user)
+
+        val db = UserDatabase.getInstance(this)
+
+        findViewById<Button>(R.id.saveBtn).setOnClickListener {
+
+            val userName:String = findViewById<EditText>(R.id.userNameEt).text.toString()
+            val phoneNumber:String = findViewById<EditText>(R.id.userPhoneNumberEt).text.toString()
+
+            if (userName.isEmpty() && phoneNumber.isEmpty()){
+                Toast.makeText(this,"Please Enter Your Name and Phone Number",Toast.LENGTH_SHORT).show()
+
+            }else{
+                val user = User(0,userName,phoneNumber)
+                GlobalScope.launch {
+                    db?.getUserDao()?.insertUser(user)
+                }
+
+                Log.d("KKKK","User is : $user")
+                startActivity(Intent(this,MainActivity::class.java))
+            }
+        }
     }
 }
--- app/src/main/java/com/example/mvvm_android/MainActivity.kt
+++ app/src/main/java/com/example/mvvm_android/MainActivity.kt
@@ -1,12 +1,32 @@
 package com.example.mvvm_android
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.mvvm_android.room_database.UserDatabase
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val db = UserDatabase.getInstance(this)
+
+        GlobalScope.launch {
+            val list = db?.getUserDao()?.loadAllUser()
+            Log.e("User", "$list")
+        }
+
+
+
+        findViewById<FloatingActionButton>(R.id.addUserFAB).setOnClickListener {
+            startActivity(Intent(this,AddUserActivity::class.java))
+        }
 
     }
 }
